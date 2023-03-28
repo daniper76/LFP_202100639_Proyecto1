7d42a530fd71d50097bfb19b3abdf41ac7884d0a
Pag <franciscoperuch76@gmai.com>
2023-03-28 04:46:30 -0600

    Tercer Commit

--- ventana.py
+++ ventana.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import subprocess
-
+import webbrowser
 ventana = tk.Tk()
 ventana.configure(bg='black',cursor="pirate")
 ventana.geometry('400x600')
@@ -12,6 +12,27 @@ def analizar():
 
 def generarArchivoJson():
     subprocess.call(['python', 'generarJson.py'])
+
+def abrir_manual_usuario():
+    webbrowser.open("https://drive.google.com/file/d/1pnb5khyTGRaxSMg0-aMnJPBGVzqBD5Rn/view?usp=sharing")
+
+def abrir_manual_tecnico():
+    webbrowser.open("https://drive.google.com/file/d/1W2HWJuiDu5LZRA1BR89RRnP55vQWByOV/view?usp=sharing")
+
+def cerrar_aplicacion():
+    ventana.quit()
+
+def ventana_ayuda():
+    ventanaAyuda=tk.Toplevel(ventana)
+    ventanaAyuda.configure(bg='black',cursor="pirate")
+    ventanaAyuda.geometry('500x500')
+    ventanaAyuda.title('Proyecto 1 LFP')
+    ventanaAyuda.resizable(0,0)
+    boton_regresar_menu = tk.Button(ventanaAyuda, text="Regresar", command=ventana.tkraise)
+    boton_regresar_menu.place(x=50,y=400)
+    etiqueta_ayuda = tk.Label(ventanaAyuda, text="Francisco Daniel Peruch de León  \nCarné: 202100639 \nLenguajes Formales y de Programación",fg="red", width=40,height=10,font=("Comic Sans MS", 15, "bold"))
+    etiqueta_ayuda.pack()
+    ventanaAyuda.mainloop()
 
 
 
@@ -65,19 +86,19 @@ boton_errores=tk.Button(ventana, text='Errores', command=generarArchivoJson, hei
 boton_errores.configure(bg='skyblue')
 boton_errores.place(x=50,y=390)
 
-boton_salir=tk.Button(ventana, text='Salir', command=analizar, height=3, width=11)
+boton_salir=tk.Button(ventana, text='Salir', command=cerrar_aplicacion, height=3, width=11)
 boton_salir.configure(bg='skyblue')
 boton_salir.place(x=50,y=510)
 
-boton_ayuda=tk.Button(ventana, text='Ayuda', command=analizar, height=3, width=11)
+boton_ayuda=tk.Button(ventana, text='Ayuda', command=ventana_ayuda, height=3, width=11)
 boton_ayuda.configure(bg='skyblue')
 boton_ayuda.place(x=250,y=50)
 
-boton_usuario=tk.Button(ventana, text='Manual de Usuario', command=analizar, height=3, width=16)
+boton_usuario=tk.Button(ventana, text='Manual de Usuario', command=abrir_manual_usuario, height=3, width=16)
 boton_usuario.configure(bg='skyblue')
 boton_usuario.place(x=250,y=270)
 
-boton_tecnico=tk.Button(ventana, text='Manual Técnico', command=analizar, height=3, width=11)
+boton_tecnico=tk.Button(ventana, text='Manual Técnico', command=abrir_manual_tecnico, height=3, width=11)
 boton_tecnico.configure(bg='skyblue')
 boton_tecnico.place(x=250,y=150)
 
