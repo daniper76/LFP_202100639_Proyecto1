bec973d7cc494a0e90dd467e94ce1642ad617826
Pag <franciscoperuch76@gmai.com>
2023-03-29 18:20:28 -0600

    Cuarto Commit

--- operaciones.py
+++ operaciones.py
@@ -4,172 +4,308 @@ class Operacion:
         self.tipo = tipo
         self.operandos = []
 
-    def operar(self):
+    def operar(self,lista_operaciones,grafo):
         res = '' # 1 + (1 + 1) = 3
         resnum = 0
+        tipo=self.tipo
         if self.tipo.lower() == 'suma':
+            
+            lista_suma=[]
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' + '
-                    resnum += float(operando)
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
+                    lista_suma.append(operando)
+                    if len(lista_suma)==2:
+                        resnum=float(lista_suma[0])+float(lista_suma[1])
+                        grafo.node(str(resnum), f'{self.tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_suma[0]))
+                        grafo.edge(str(resnum),str(lista_suma[1]))
+                        
                 else:
-                    operado = operando.operar()
+                    operado = operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') + '
-                    resnum += operado[1]
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
+                    lista_suma.append(operado[1])
+                    if len(lista_suma)==2:
+                        resnum=float(lista_suma[0])+float(lista_suma[1])
+                        grafo.node(str(resnum), f'{self.tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_suma[0]))
+                        grafo.edge(str(resnum),str(lista_suma[1]))
+                    
 
         if self.tipo.lower() == 'resta':
             lista_resta=[]
+        
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' - '
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
                     lista_resta.append(operando)
+                    lista_operaciones.append(float(operando))
                     if len(lista_resta)==2:
                         resnum=float(lista_resta[0])-float(lista_resta[1])
+                        grafo.node(str(resnum),f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_resta[0]))
+                        grafo.edge(str(resnum),str(lista_resta[1]))
                 else:
-                    operado = operando.operar()
+                    operado = operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') - '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_resta.append(operado[1])
                     if len(lista_resta)==2:
                         resnum=float(lista_resta[0])-float(lista_resta[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_resta[0]))
+                        grafo.edge(str(resnum),str(lista_resta[1]))
 
 
         elif self.tipo.lower() == 'multiplicacion':
             resnum = 1
+            lista_multiplicacion=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' * '
-                    resnum = resnum * float(operando)
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
+                    lista_multiplicacion.append(operando)
+                    if len(lista_multiplicacion)==2:
+                        resnum = float(lista_multiplicacion[0])*float(lista_multiplicacion[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_multiplicacion[0]))
+                        grafo.edge(str(resnum),str(lista_multiplicacion[1]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') * '
-                    resnum = resnum * float(operado[1])
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
+                    lista_multiplicacion.append(operado[1])
+                    if len(lista_multiplicacion)==2:
+                        resnum = float(lista_multiplicacion[0])*float(lista_multiplicacion[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_multiplicacion[0]))
+                        grafo.edge(str(resnum),str(lista_multiplicacion[1]))
 
         elif self.tipo.lower() == 'division':
             resnum = 1
             lista_division=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' / '
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_division.append(operando)
                     if len(lista_division)==2:
                         resnum=float(lista_division[0])/float(lista_division[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_division[0]))
+                        grafo.edge(str(resnum),str(lista_division[1]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') / '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_division.append(float(operado[1]))
                     if len(lista_division)==2:
                         resnum=float(lista_division[0])/float(lista_division[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_division[0]))
+                        grafo.edge(str(resnum),str(lista_division[1]))
 
         elif self.tipo.lower() == 'potencia':
             resnum = 1
             lista_potencia=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' ^ '
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_potencia.append(operando)
                     if len(lista_potencia)==2:
                         resnum=float(lista_potencia[0])**float(lista_potencia[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_potencia[0]))
+                        grafo.edge(str(resnum),str(lista_potencia[1]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') ^ '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_potencia.append(float(operado[1]))
                     if len(lista_potencia)==2:
-                        resnum=float(lista_division[0])**float(lista_division[1])
+                        resnum=float(lista_potencia[0])**float(lista_potencia[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_potencia[0]))
+                        grafo.edge(str(resnum),str(lista_potencia[1]))
 
         elif self.tipo.lower() == 'raiz':
             resnum = 1
             lista_raiz=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' ^0.5 '
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_raiz.append(operando)
                     if len(lista_raiz)==1:
                         resnum=float(lista_raiz[0])**float(0.5)
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_raiz[0]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') ^ (1/2) '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_raiz.append(operado[1])
                     if len(lista_raiz)==1:
                         resnum=float(lista_raiz[0])**float(0.5)
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_raiz[0]))
 
         elif self.tipo.lower() == 'inverso':
             resnum = 1
+            
             lista_inverso=[]
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += 'Inv('+operando+')'
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_inverso.append(operando)
                     if len(lista_inverso)==1:
                         resnum=float(1/lista_inverso[0])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_inverso[0]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += 'Inverso(' + operado[0] + ') '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_inverso.append(operado[1])
                     if len(lista_inverso)==1:
                         resnum=float(1/lista_inverso[0])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_inverso[0]))
 
         elif self.tipo.lower() == 'seno':
             resnum = 1
             lista_seno=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += 'Sen('+operando+')'
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_seno.append(operando)
                     if len(lista_seno)==1:
                         resnum=float(math.sin(math.radians(float(lista_seno[0]))))
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_seno[0]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += 'Sen(' + operado[0] + ') '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_seno.append(operado[1])
                     if len(lista_seno)==1:
                         resnum=float(math.sin(math.radians(float(lista_seno[0]))))
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_seno[0]))
 
         elif self.tipo.lower() == 'coseno':
             resnum = 1
             lista_coseno=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += 'Cos('+operando+')'
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_coseno.append(operando)
                     if len(lista_coseno)==1:
                         resnum=float(math.cos(math.radians(float(lista_coseno[0]))))
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_coseno[0]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += 'Cos(' + operado[0] + ') '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_coseno.append(operado[1])
                     if len(lista_coseno)==1:
                         resnum=float(math.cos(math.radians(float(lista_coseno[0]))))
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_coseno[0]))
 
         elif self.tipo.lower() == 'tangente':
             resnum = 1
             lista_tangente=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += 'Tan('+operando+')'
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_tangente.append(operando)
                     if len(lista_tangente)==1:
                         resnum=float(math.tan(math.radians(float(lista_tangente[0]))))
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_tangente[0]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += 'Tan(' + operado[0] +')'
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_tangente.append(operado[1])
                     if len(lista_tangente)==1:
                         resnum=float(math.tan(math.radians(float(lista_tangente[0]))))
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_tangente[0]))
 
         elif self.tipo.lower() == 'mod':
             resnum = 1
             lista_mod=[]
+            
             for operando in self.operandos:
                 if type(operando) is not Operacion:
                     res += operando + ' mod '
+                    grafo.node(str(operando), f'{operando}', shape="doublecircle")
+                    lista_operaciones.append(float(operando))
                     lista_mod.append(operando)
                     if len(lista_mod)==2:
                         resnum=float(lista_mod[0])%float(lista_mod[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_mod[0]))
+                        grafo.edge(str(resnum),str(lista_mod[1]))
                 else:
-                    operado=operando.operar()
+                    operado=operando.operar(lista_operaciones,grafo)
                     res += '(' + operado[0] + ') / '
+                    grafo.node(str(operado[1]), f'{operado[1]}', shape="doublecircle")
+                    lista_operaciones=operado[2]
+                    grafo=operado[3]
                     lista_mod.append(float(operado[1]))
                     if len(lista_mod)==2:
                         resnum=float(lista_mod[0])%float(lista_mod[1])
+                        grafo.node(str(resnum), f'{tipo}\n'+f'{resnum}', shape="doublecircle")
+                        grafo.edge(str(resnum),str(lista_mod[0]))
+                        grafo.edge(str(resnum),str(lista_mod[1]))
 
-        return [res[0:-3], resnum]
+        lista_operaciones.append(resnum)
+        return [res[0:-3], resnum,lista_operaciones,grafo]
--- prueba77.py
+++ prueba77.py
@@ -1,6 +1,8 @@
 from operaciones import Operacion
 from Token import Token
 from analizadorPrueba import*
+import graphviz 
+import math
 
 def comenzar():
     autom = Automata()
@@ -9,12 +11,126 @@ def comenzar():
     resultado = autom.analizar(cadena , Operacion('sexo'))
     autom.imprimir_tokens()
     autom.imprimir_errores()
+    lista_operaciones=[]
+    grafo = graphviz.Digraph('ejemplo', filename="grafo");
+    grafo.attr(rankdir="TB")
+
     if autom.estado_actual in autom.estados_aceptacion:
         for oper in resultado[1]:
             try:
-                resultado = oper.operar()
+                resultado = oper.operar(lista_operaciones,grafo)
                 print(resultado[0], "=", resultado[1])
+                lista_operaciones=resultado[2]
+                grafo=resultado[3]
             except Exception as e:
-                print("Hubo un error LPTM!!! :/ :(")
+                print("Hubo un error")
+    print(lista_operaciones)
+    grafo.view()
+    '''lista_operacion_igualar=lista_operaciones
+    lista_operacion_resultados=lista_operaciones
+
+    grafo = graphviz.Digraph('ejemplo', filename="grafo");
+    grafo.attr(rankdir="TB")
+    indice=0
+    for numero in lista_operaciones:
+        grafo.node(str(indice), f'{numero}', shape="doublecircle")
+        indice+=1
+
+    indice1=0
+    for num in lista_operaciones:
+        indice2=0
+        for num2 in lista_operacion_igualar:
+            if num!=num2:
+
+                if (float(num)+float(num2)) in lista_operacion_resultados:
+                    indice3=0
+                    for num3 in lista_operacion_resultados:
+                        if (float(num)+float(num2)==float(num3)):
+                            if f'{indice3} -> {indice1}' not in grafo.body and f'{indice3} -> {indice2}' not in grafo.body:
+                                grafo.edge(str(indice3),str(indice1))
+                                grafo.edge(str(indice3),str(indice2))
+                        indice3+=1
+                if (float(num)-float(num2)) in lista_operacion_resultados:
+                    indice3=0
+                    for num3 in lista_operacion_resultados:
+                        if (float(num)-float(num2)==float(num3)):
+                            if f'{indice3} -> {indice1}' not in grafo.body and f'{indice3} -> {indice2}' not in grafo.body:
+                                grafo.edge(str(indice3),str(indice1))
+                                grafo.edge(str(indice3),str(indice2))
+                        indice3+=1
+                if (float(num)*float(num2)) in lista_operacion_resultados:
+                    indice3=0
+                    for num3 in lista_operacion_resultados:
+                        if (float(num)*float(num2)==float(num3)):
+                            if f'{indice3} -> {indice1}' not in grafo.body and f'{indice3} -> {indice2}' not in grafo.body:
+                                grafo.edge(str(indice3),str(indice1))
+                                grafo.edge(str(indice3),str(indice2))
+                        indice3+=1
+                if (float(num)/float(num2)) in lista_operacion_resultados:
+                    indice3=0
+                    for num3 in lista_operacion_resultados:
+                        if (float(num)/float(num2)==float(num3)):
+                            if f'{indice3} -> {indice1}' not in grafo.body and f'{indice3} -> {indice2}' not in grafo.body:
+                                grafo.edge(str(indice3),str(indice1))
+                                grafo.edge(str(indice3),str(indice2))
+                        indice3+=1
+                if (float(num)**float(num2)) in lista_operacion_resultados:
+                    indice3=0
+                    for num3 in lista_operacion_resultados:
+                        if (float(num)**float(num2)==float(num3)):
+                            if f'{indice3} -> {indice1}' not in grafo.body and f'{indice3} -> {indice2}' not in grafo.body:
+                                grafo.edge(str(indice3),str(indice1))
+                                grafo.edge(str(indice3),str(indice2))
+                        indice3+=1
+                if (float(num)%float(num2)) in lista_operacion_resultados:
+                    indice3=0
+                    for num3 in lista_operacion_resultados:
+                        if (float(num)%float(num2)==float(num3)):
+                            if f'{indice3} -> {indice1}' not in grafo.body and f'{indice3} -> {indice2}' not in grafo.body:
+                                grafo.edge(str(indice3),str(indice1))
+                                grafo.edge(str(indice3),str(indice2))
+                        indice3+=1
+            indice2+=1
+
+        
+        if (float(num)**float(0.5)) in lista_operacion_resultados:
+            indice4=0
+            for num4 in lista_operacion_resultados:
+                if (float(num)**float(0.5)==float(num4)):
+                    if f'{indice4} -> {indice1}' not in grafo.body:
+                        grafo.edge(str(indice4),str(indice1))
+                indice4+=1
+        if (1/float(num)) in lista_operacion_resultados:
+            indice4=0
+            for num4 in lista_operacion_resultados:
+                if (1/float(num)==float(num4)):
+                    if f'{indice4} -> {indice1}' not in grafo.body:
+                        grafo.edge(str(indice4),str(indice1))
+                indice4+=1
+        if (float(math.sin(math.radians(float(num))))) in lista_operacion_resultados:
+            indice4=0
+            for num4 in lista_operacion_resultados:
+                if (float(math.sin(math.radians(float(num))))==float(num4)):
+                    if f'{indice4} -> {indice1}' not in grafo.body:
+                        grafo.edge(str(indice4),str(indice1))
+                indice4+=1
+        if (float(math.cos(math.radians(float(num))))) in lista_operacion_resultados:
+            indice4=0
+            for num4 in lista_operacion_resultados:
+                if (float(math.cos(math.radians(float(num))))==float(num4)):
+                    if f'{indice4} -> {indice1}' not in grafo.body:
+                        grafo.edge(str(indice4),str(indice1))
+                indice4+=1
+        if (float(math.tan(math.radians(float(num))))) in lista_operacion_resultados:
+            indice4=0
+            for num4 in lista_operacion_resultados:
+                if (float(math.tan(math.radians(float(num))))==float(num4)):
+                    if f'{indice4} -> {indice1}' not in grafo.body:
+                        grafo.edge(str(indice4),str(indice1))
+                indice4+=1
+        indice1+=1
+        
+    grafo.view()'''
+
 
 comenzar()
